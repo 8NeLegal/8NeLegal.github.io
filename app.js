6132fbd3dbd94f77f1f8ef1ca38fbb4031ad7e70
8NeLegal <143360559+8NeLegal@users.noreply.github.com>
2023-10-27 23:27:35 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
