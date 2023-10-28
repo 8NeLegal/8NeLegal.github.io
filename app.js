5adc34a35fe2b63b4e2518acdbfb465058c0c6b9
8NeLegal <143360559+8NeLegal@users.noreply.github.com>
2023-10-29 02:05:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ let tg = window.Telegram.WebApp;
 let start = document.getElementById("start");
 let end = document.getElementById("end");
 
+tg.expand();
+
+
 start.addEventListener("click", () => {
     document.getElementById("main").style.display = "none";
     document.getElementById("form_data").style.display = "block";
 });
 
 
+end.addEventListener("click", () => {
+    document.getElementById("form_data").style.display = "none";
+    document.getElementById("form_send").style.display = "block";
+});
 
 
-tg.expand();
+
+
 
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 let item = "";
-
-
-
-
 
 
 
