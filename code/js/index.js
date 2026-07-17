765fc1197698cbcf270917503cddd0e874477c32
4or7una <143360559+8NeLegal@users.noreply.github.com>
2026-07-17 23:04:57 +0700

    Add files via upload

---
+++ code/js/index.js
@@ -0,0 +1,6 @@
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+const minWidth = 1920;
+const minHeight = 1080;
+
+if (screenWidth >= minWidth && screenHeight >= minHeight) {window.location = "code/html/main.html"}
